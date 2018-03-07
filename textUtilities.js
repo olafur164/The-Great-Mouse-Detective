87f3b64d27f35aa3f2d9f7c3b284faafb53c0729
Ólafur Hólm <olafurholm17@gmail.com>
2018-03-07 13:34:08 +0000

    First test run

---
+++ textUtilities.js
@@ -0,0 +1,14 @@
+const expect = require("chai").expect;
+
+
+// expect(true).to.be.true;
+
+titleCase = (title) => {
+  const words = title.split(" ");
+  let titleCasedWords = words.map(function (word) {
+    return word[0].toUpperCase() + word.substring(1);
+  });
+  return titleCasedWords.join(" ");
+}
+
+expect(titleCase("the great mouse detective")).to.be.a("string");
