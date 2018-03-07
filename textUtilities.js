49b44bec6250de937402e53338700e8596abf75e
Ólafur Hólm <olafurholm17@gmail.com>
2018-03-07 13:37:21 +0000

    Fourth Test run

--- textUtilities.js
+++ textUtilities.js
@@ -14,4 +14,5 @@ titleCase = (title) => {
 expect(titleCase("the great mouse detective")).to.be.a("string");
 expect(titleCase("a")).to.equal("A")
 expect(titleCase("vertigo")).to.equal("Vertigo");
+expect(titleCase("the great mouse detective")).to.equal("The Great Mouse Detective");
 
